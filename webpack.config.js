704fd5c56470d4e04a2661debfd21982b65bd2c7
Nolleschorsch <elnerdo2@web.de>
2023-12-27 09:47:43 +0100

    FUCK THAT SHIT

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname),
         filename: './bundle.js',
         publicPath: '/'
     },
