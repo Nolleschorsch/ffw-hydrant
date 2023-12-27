74672e38f791d1cc6ca2562eb28f90341e9047fb
Nolleschorsch <elnerdo2@web.de>
2023-12-27 11:35:29 +0100

    whatever

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ async function enableMocking() {
     /* if (process.env.NODE_ENV !== 'development' || process.env.MY_ENV_VAR === 'Foo') {
         return
     } */
-    
+
     //const { worker } = await import('./features/map/mocks/browser')
     const { worker } = await import('./mocks/browser')
 
     // `worker.start()` returns a Promise that resolves
     // once the Service Worker is up and ready to intercept requests.
     // TODO: use flag so this still works in dev
-    return worker.start({onUnhandledRequest: "bypass", url: './mockServiceWorker.js'})
+    return worker.start({ onUnhandledRequest: "bypass", serviceWorker: { url: './mockServiceWorker.js' } })
 }
 
 const container = document.getElementById('root')
