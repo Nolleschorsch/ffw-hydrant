1f12807ce5551818db43f69d2f6b51106a0d7f43
Nolleschorsch <elnerdo2@web.de>
2023-12-27 09:40:38 +0100

    losing my patience...

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+        filename: './bundle.js',
         publicPath: '/'
     },
     resolve: {
