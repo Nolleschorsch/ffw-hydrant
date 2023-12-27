3dd1d91079968c2b515e0997a5de32714158d39a
Nolleschorsch <elnerdo2@web.de>
2023-12-27 07:48:35 +0100

    troublehshooting gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
     output: {
         //path: path.resolve(__dirname, 'build'),
-        path: '/',
+        path: path.resolve(__dirname),
         filename: 'bundle.js',
         publicPath: '/'
     },
