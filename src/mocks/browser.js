210d11fe6b2870f440dbebc47388dd5bb9d4a040
Nolleschorsch <elnerdo2@web.de>
2023-12-27 08:58:51 +0000

    Deploying to gh-pages from @ Nolleschorsch/ffw-hydrant@f4dcc5ba47285ccb73e8038ee7ff0ba18d1cae89 🚀

--- src/mocks/browser.js
+++
@@ -1,7 +0,0 @@
-import { setupWorker } from 'msw/browser'
-//import { handlers, external_handlers } from './handler'
-import handlers from './handlers' 
-
-//export const worker = setupWorker(...handlers.external_handlers)
-//export const worker = setupWorker(...handlers.all_handlers)
-export const worker = setupWorker(...handlers.drf_handlers)
